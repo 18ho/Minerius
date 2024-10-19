231b7addf2d2a30e79fea208049586d738076a39
JinhyungPark <jinh509@naver.com>
2024-10-19 16:21:43 +0900

    수정

---
+++ minerius/pages/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            <Text>Hello, World!</Text>
+            
+        </View>
+    );
+};
+
+export default Main;
